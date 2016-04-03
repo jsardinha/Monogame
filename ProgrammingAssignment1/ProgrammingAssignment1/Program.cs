67aa91833d52e0e373840a29f4791bd189e366dc
jsardinha <jsardinha@users.noreply.github.com>
2016-04-03 03:41:08 +0100

    Final

--- ProgrammingAssignment1/ProgrammingAssignment1/Program.cs
+++ ProgrammingAssignment1/ProgrammingAssignment1/Program.cs
@@ -21,38 +21,39 @@ namespace ProgrammingAssignment1
             Console.WriteLine();
             Console.WriteLine("Welcome.\n\n" +
                               "This program will calculate both distance\n" +
-                              "and angle between to points of your choosing.\n\n");
+                              "and angle between two points of your choosing.\n\n");
 
             // enter coordinates
             Console.WriteLine("Point 1:");
             Console.WriteLine("========");
             Console.Write("Enter X coordinate: ");
-            double point1X = double.Parse(Console.ReadLine());
+            float point1X = float.Parse(Console.ReadLine());
             Console.Write("Enter Y coordinate: ");
-            double point1Y = double.Parse(Console.ReadLine());
+            float point1Y = float.Parse(Console.ReadLine());
             Console.WriteLine();
             Console.WriteLine("Point 2: ");
             Console.WriteLine("========");
             Console.Write("Enter X coordinate: ");
-            double point2X = double.Parse(Console.ReadLine());
+            float point2X = float.Parse(Console.ReadLine());
             Console.Write("Enter Y coordinate: ");
-            double point2Y = double.Parse(Console.ReadLine());
+            float point2Y = float.Parse(Console.ReadLine());
             Console.WriteLine();
 
             // calcultate delta
-            double deltaX = point2X - point1X;
-            double deltaY = point2Y - point1Y;
+            float deltaX = point2X - point1X;
+            float deltaY = point2Y - point1Y;
 
             // calculate distance using Pythagorean Theorem
-            double distance = Math.Sqrt((deltaX * deltaX) + (deltaY * deltaY));
+            float distance = (float)Math.Sqrt((deltaX * deltaX) + (deltaY * deltaY));
 
             // calculate angle in degrees
-            double angle = Math.Atan2(deltaY, deltaX) * (180 / Math.PI);
+            float angle = (float)(Math.Atan2(deltaY, deltaX) * (180 / Math.PI));
 
             // print results
-            Console.WriteLine("The distance between the two points is: " + Math.Round(distance, 3));
             Console.WriteLine();
-            Console.WriteLine("The angle between the two points is " + Math.Round(angle, 3) + " degrees.");
+            Console.WriteLine("Distance between points: " + distance.ToString("F3"));
+            Console.WriteLine();
+            Console.WriteLine("Angle between points: " + angle.ToString("F3") + " degrees.");
             Console.WriteLine();
         }
     }
